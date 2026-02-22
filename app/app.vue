ea211b78638a2814df526fccf7ce80cfe3bab657
Izbergenov Dauletkhan <izbergenovdauletkhan@MacBook-Pro-Izbergenov.local>
2026-02-22 23:54:57 +0500

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <u-app>
+    <nuxt-page />
+  </u-app>
+</template>
+<style src="~/assests/css/main.css"></style>
---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <u-container>
+    <u-button variant="link">
+      
+      Button
+    </u-button>
+  </u-container> 
+</template>
+
+<style scoped></style>
