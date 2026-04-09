bbfa3909b322e42bbd579b2879fd594cc09ae643
Dauletkhan Izbergen <di.izbergen@gmail.com>
2026-04-09 16:15:13 +0500

    (feat::correction) init plugin

--- app/plugins/init.plugin.ts
+++ app/plugins/init.plugin.ts
@@ -2,18 +2,11 @@ export default defineNuxtPlugin({
   name: 'init',
   dependsOn: ['api'],
   parallel: true,
-  async setup(app){
+  async setup(){
     const authStore = useAuthStore();
 
     if(import.meta.browser){
         await authStore.initSession();
-
-      app.hook('app:suspense:resolve', function(){
-        authStore.hydrated();
-      });
-    }
-    else{
-      authStore.user = null;
     }
   }
 });
